11d05a676aa0dc25e801db00a7d55e25baee445d
vishalbarot <vishalbarot@ymail.com>
2023-02-18 12:03:19 -0600

    Empty Spring boot project for Motorcycle service

---
+++ src/test/java/com/vishalbarot/motorcycleservice/MotorcycleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishalbarot.motorcycleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorcycleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
